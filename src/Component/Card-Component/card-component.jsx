b42e3764d6d24c72eab96e778e33fb6358fc64ab
Hanzala Saeed <hanzalasaeed47@gmail.com>
2020-03-19 22:46:07 +0500

    gave cards all structure. with grids

---
+++ src/Component/Card-Component/card-component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './card-list.styles.css';
+import { Card } from '../Card/card';
+
+export const CardList = props => (
+    <div className="card-list">
+        {
+
+            props.monsters.map(monster => (<Card key={monster.id} monster={monster} />))
+
+
+        }
+
+    </div>
+);
