6fdbfaa2a920093b6612470e89711eab98f0bf30
Hanzala Saeed <hanzalasaeed47@gmail.com>
2020-03-20 10:23:58 +0500

    Updates

---
+++ precache-manifest.bac4fff2429246016f55acbf66ab1140.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a72db383f89306323d0dc40f7f576814",
+    "url": "/Monster-Rolodox/index.html"
+  },
+  {
+    "revision": "9fc3ada99f7fed5911ea",
+    "url": "/Monster-Rolodox/static/css/main.d09e5dfd.chunk.css"
+  },
+  {
+    "revision": "e155e68cf5d10890ecb5",
+    "url": "/Monster-Rolodox/static/js/2.e5f7a9ee.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Monster-Rolodox/static/js/2.e5f7a9ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fc3ada99f7fed5911ea",
+    "url": "/Monster-Rolodox/static/js/main.c41c62c6.chunk.js"
+  },
+  {
+    "revision": "160fe411878ada782893",
+    "url": "/Monster-Rolodox/static/js/runtime-main.e43f7626.js"
+  }
+]);
